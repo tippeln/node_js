64fd15751aba61f5058eda6809b5be537d3ac4dc
tippeln <tippeln@gmail.com>
2022-12-09 10:57:29 +0900

    ログイン機能修正（関数化など）

--- routes/login.js
+++ routes/login.js
@@ -4,96 +4,72 @@ const db = require('../models/index');
 const { Op } = require("sequelize");
 const bcrypt = require('bcrypt');
 
-/* GET home page. */
+/* GET */
 router.get('/', (req, res, next) => {
     if (req.session.username) {
         res.redirect('/');
       } else {
-        var data = {
-          title: 'ログイン',
-          func:'login',
-          username: req.session.username,
-          lastdate: req.session.lastdate,
-          iderr:'　　',
-          pwderr:'　　',
-          func:'login'
-        }
-        res.render('login',　data);
+          var data = {
+            title: 'ログイン',
+            func:'login',
+            username: '',
+            lastdate: '',
+            errmsg:'',
+            userid: ''
+          }
+          res.render('login', data);       
       }
 });
 
+/* Psot */
 router.post('/', (req, res, next) => {
+    //フォーム情報取得
     var userid = req.body.userid || null;
     var password = req.body.password || null;
-    console.log('user:' + userid);
 
     if(!userid || !password) {
-      res.statusCode = 403;
-      var data = {
-        title: 'ログイン',
-        func:'login',
-        username: req.session.username,
-        lastdate: req.session.lastdate,
-        iderr:'ユーザIDを入力してください。',
-        pwderr:'パスワードを入力してください。',
-        func:'login'
-      }
-      res.render('login',　data);
+      wrongForm(req, res, userid);      
     }
     //ユーザーマスタ検索
       db.m_user.findOne({
         where:{user_id:req.body.userid}
       }).then(usr=>{
         if (usr != null) {
-          if(bcrypt.compareSync(req.body.password, usr.pwd)) {
-            req.session.username = usr.user_nm;
-            const date = new Date();
-            req.session.lastdate = date.toLocaleString();
-            console.log('login ok!');
-            //最終ログオン日時の更新
-            db.m_user.update({
-              last_logon_datetime:date
-            },
-            {
-              where:{user_id:req.body.userid}
-            })
-            res.redirect('/');
-          }
+            //パスワードチェック
+            if(bcrypt.compareSync(req.body.password, usr.pwd)) {
+                //セッション情報の保存
+                req.session.username = usr.user_nm;
+                const date = new Date();
+                req.session.lastdate = date.toLocaleString();
+
+                //最終ログオン日時の更新
+                db.m_user.update({
+                  last_logon_datetime:date
+                },
+                {
+                  where:{user_id:req.body.userid}
+                })
+                res.redirect('/');
+            } else {
+              wrongForm(req, res, userid);          
+            }
         } else {
-          var data = {
-            title: 'ログイン',
-            func:'login',
-            username: req.session.username,
-            lastdate: req.session.lastdate,
-            iderr:'ユーザIDかパスワードに問題があります。再度入力下さい。',
-            pwderr:'　　',
-            func:'login'
-          }
-          res.statusCode = 403;
-          res.render('login', data);
+          wrongForm(req, res, userid);
         }
       })
     });
-    module.exports = router;
 
+    function wrongForm(req, res, userid) {
+        var data = {
+          title: 'ログイン',
+          func:'login',
+          username: '',
+          lastdate: '',
+          errmsg:'ユーザIDかパスワードに問題があります。再度入力下さい。',
+          userid: userid
+        }
+        res.statusCode = 403;
+        res.render('login', data);     
+    }
 
-
-
-
- /*   db.User.findOne({
-      where: {
-          name:{[Op.eq]:req.body.userid},
-          pass:{[Op.eq]:req.body.password}
-      }
-    }).success(usrs => {
-        req.session.username = 'usrs';
-        req.session.lastdate = new Date();
-        console.log('login ok!')
-        res.redirect('/');
-    }) 
-    .error(usrs => {
-      res.statusCode = 403;
-      console.log('login NG!')
-      res.redirect('/login');
-    }); 
-});*/
+module.exports = router;
--- routes/logout.js
+++ routes/logout.js
@@ -3,13 +3,6 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-/*   var data = {
-      title: 'logout',
-      func:'common',
-      username: req.session.username,
-      lastdate: req.session.lastdate
-  }*/
-//  res.render('logout', data);
   req.session.destroy((err) => {
     res.redirect('/login');
   });
