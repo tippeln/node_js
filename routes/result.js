3f661a73338fcdecf06e2b0de62f85d02b216365
tippeln <tippeln@gmail.com>
2022-12-12 21:51:57 +0900

    修正

--- routes/result.js
+++ routes/result.js
@@ -4,18 +4,20 @@ const db = require('../models/index');
 const { Op } = require("sequelize");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  var data = {
-      title: '取り込み結果確認画面',      
+router.get('/',(req, res, next)=> {
+  db.t_error.findAll().then(usrs => {
+    var data = {
+      title: '取り込み結果確認画面',
       func:'result',
       username: req.session.username,
-      lastdate: req.session.lastdate
-  }
-  res.render('result', data);
+      lastdate: req.session.lastdate,
+      content: usrs
+    }
+    res.render('result', data);
+  });
 });
 
-/* GET users listing. */
-router.get('/execute', function(req, res, next) {
+router.get('/execute', (req, res, next) => {
     var data = {
         title: '登録実行画面',        
         func:'common',
@@ -25,27 +27,11 @@ router.get('/execute', function(req, res, next) {
     res.render('execute', data);
   });
 
-  /* GET users listing. */
-/*router.get('/errorcheck', function(req, res, next) {
-  const no = "備考欄です。" ;
-
-  db.t_error.findAll({
-    where:{biko: no}
-  }).then(usrs =>{
-    console.log('row_no???:'+ usrs.row_no)
-    var data = {
-        title: 'エラー確認画面',
-        func:'errorcheck',
-        username: req.session.username,
-        lastdate: req.session.lastdate,
-        no:usrs.row_no
-    }
-    res.render('errorcheck', data);
-  })
-});*/
-
 router.get('/errorcheck',(req, res, next)=> {
-  db.t_error.findAll().then(usrs => {
+  const id = 1;
+  db.t_error.findAll({
+    where: { id:id},
+  }).then(usrs => {
     var data = {
       title: 'エラー確認画面',
       func:'errorcheck',
@@ -56,14 +42,5 @@ router.get('/errorcheck',(req, res, next)=> {
     res.render('errorcheck', data);
   });
 });
-  /*router.get('/errorcheck', function(req, res, next) {
-    var data = {
-        title: 'エラー確認画面',
-        func:'errorcheck',
-        username: req.session.username,
-        lastdate: req.session.lastdate
-    }
-    res.render('errorcheck', data);
-  });*/
 
 module.exports = router;
